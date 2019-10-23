8e84ad4119405aebc46d71c1e5b892f3f776ff84
elvismariel <elvismariel@gmail.com>
2019-10-23 10:03:59 -0300

    Estrutura do projeto de backend

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors"); // Define quem poderá acessar a aplicação.
+
+const app = express();
+
+app.use(cors());
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+io.on("connection", socket => {
+    socket.on("connectRoom", box => {
+        socket.join(box);
+    })
+});
+
+mongoose.connect('mongodb://localhost/dropbox', {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => {
+    console.log('Conectado ao mongo com sucesso!');
+}).catch((erro) => {
+    console.log('Ocorreu um erro ao se conectar com o mongodb: ' + erro);
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+
+    return next();
+});
+
+app.use(express.json());    // Define que vai trabalhar com Json
+app.use(express.urlencoded({extended: true }));  // Define que vai ser usado arquivos de upload
+app.use(require('./routes')); // Define a importacao do arquivo de rotas
+app.use("/files", express.static(path.resolve(__dirname, "..","tmp"))); // Libera acesso aos arquivos staticos da pasta tmp
+
+server.listen( process.env.PORT || 3333);
